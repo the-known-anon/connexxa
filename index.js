76dfdd17d720b631352c7ca84bbe106144b7f832
usercspa <spacchbk@gmail.com>
2020-12-06 04:33:40 +0000

    1 file modified

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 'use strict';
  
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
 const admin = require('firebase-admin');
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'ws://connexbot-sfb9-default-rtdb.firebaseio.com/',
-});
-admin.initializeApp();
-const db = admin.firestore();
+const {WebhookClient} = require('dialogflow-fulfillment');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
+const db = admin.firestore();
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
  
   function getBgHandler(agent) {
-    let bg = agent.parameters.name || agent.context.get('Starthealthcheck-followup').parameters.name;
-    db.collection('bg').add({ bg: bg });
-    agent.add(`Your blood glucose level is ${bg}`);
+    const bg = agent.parameters.bg;
+    const dialogflowAgentRef = db.collection('bg').doc('bg');
+	return db.runWrite(w => {
+      w.set(dialogflowAgentRef, {entry: bg});
+      return Promise.resolve('Write complete');
+    }).then(doc => {
+      bg.add(`Wrote "${bg}" to the Firestore database.`);
+    }).catch(err => {
+      console.log(`Error writing to Firestore: ${err}`);
+      bg.add(`Failed to write "${bg}" to the Firestore database.`);
+    });
   }
   
  function getSbpHandler(agent) {
-    let Sbp = agent.parameters.name || agent.context.get('Starthealthcheck-custom-followup').parameters.name; 
-   	db.collection('Sbp').add({ Sbp: Sbp });
-    agent.add(`Your systolic blood pressure is ${Sbp}`);
-  }
+    const Sbp = agent.parameters.Sbp;
+    const dialogflowAgentRef1 = db.collection('Sbp').doc('Sbp');
+	return db.runWrite(w => {
+      w.set(dialogflowAgentRef1, {entry: Sbp});
+      return Promise.resolve('Write complete');
+    }).then(doc => {
+      Sbp.add(`Wrote "${Sbp}" to the Firestore database.`);
+    }).catch(err => {
+      console.log(`Error writing to Firestore: ${err}`);
+      Sbp.add(`Failed to write "${Sbp}" to the Firestore database.`);
+    });
+  } 
  
+   function getDbpHandler(agent) {
+    const Dbp = agent.parameters.Dbp;
+    const dialogflowAgentRef2 = db.collection('Dbp').doc('Dbp');
+	return db.runWrite(w => {
+      w.set(dialogflowAgentRef2, {entry: Dbp});
+      return Promise.resolve('Write complete');
+    }).then(doc => {
+      Dbp.add(`Wrote "${Dbp}" to the Firestore database.`);
+    }).catch(err => {
+      console.log(`Error writing to Firestore: ${err}`);
+      Dbp.add(`Failed to write "${Dbp}" to the Firestore database.`);
+    });
+  } 
   
-  function getDbpHandler(agent) {
-    let Dbp = agent.parameters.name || agent.context.get('Starthealthcheck-custom-custom-followup').parameters.name; 
-    db.collection('Dbp').add({ Dbp: Dbp });
-    agent.add(`Your diastolic blood pressure is ${Dbp}`);
-  }
   
   let intentMap = new Map();
   intentMap.set('bgGET', getBgHandler);
