bac5e65f0244e3e6e2a868a0f734cc4b972183af
usercspa <spacchbk@gmail.com>
2020-12-06 11:56:01 +0000

    2 files modified

--- index.js
+++ index.js
@@ -20,47 +20,23 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
  
   function getBgHandler(agent) {
     const blood_glucose = agent.parameters.blood_glucose;
-    const dialogflowAgentRef = db.collection('blood_glucose').doc('blood_glucose');
-	return db.runWrite(w => {
-      w.set(dialogflowAgentRef, {entry: blood_glucose});
-      return Promise.resolve('Write complete');
-    }).then(doc => {
-      blood_glucose.add(`Wrote "${blood_glucose}" to the Firestore database.`);
-    }).catch(err => {
-      console.log(`Error writing to Firestore: ${err}`);
-      blood_glucose.add(`Failed to write "${blood_glucose}" to the Firestore database.`);
+    return admin.database().ref('data').set({
+      blood_glucose: '100',
     });
   }
-  
+ 
  function getSbpHandler(agent) {
     const systolic_bp = agent.parameters.systolic_bp;
-    const dialogflowAgentRef1 = db.collection('systolic_bp').doc('systolic_bp');
-	return db.runWrite(w => {
-      w.set(dialogflowAgentRef1, {entry: systolic_bp});
-      return Promise.resolve('Write complete');
-    }).then(doc => {
-      systolic_bp.add(`Wrote "${systolic_bp}" to the Firestore database.`);
-    }).catch(err => {
-      console.log(`Error writing to Firestore: ${err}`);
-      systolic_bp.add(`Failed to write "${systolic_bp}" to the Firestore database.`);
+    return admin.database().ref('data').set({
+      systolic_bp: '120',
     });
-  } 
- 
+ }
    function getDbpHandler(agent) {
     const diastolic_bp = agent.parameters.diastolic_bp;
-    const dialogflowAgentRef2 = db.collection('diastolic_bp').doc('diastolic_bp');
-	return db.runWrite(w => {
-      w.set(dialogflowAgentRef2, {entry: diastolic_bp});
-      return Promise.resolve('Write complete');
-    }).then(doc => {
-      diastolic_bp.add(`Wrote "${diastolic_bp}" to the Firestore database.`);
-    }).catch(err => {
-      console.log(`Error writing to Firestore: ${err}`);
-      diastolic_bp.add(`Failed to write "${diastolic_bp}" to the Firestore database.`);
+    return admin.database().ref('data').set({
+      diastolic_bp: '80',
     });
-  } 
-  
-  
+   }
   let intentMap = new Map();
   intentMap.set('bgGET', getBgHandler);
   intentMap.set('systolicGET', getSbpHandler);
