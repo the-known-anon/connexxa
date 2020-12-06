ad49bf99031efc4c78039379523835d0372c8c5d
usercspa <spacchbk@gmail.com>
2020-12-06 04:08:50 +0000

    2 files modified

--- index.js
+++ index.js
@@ -2,46 +2,41 @@
  
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
-
-//connect to database
 const admin = require('firebase-admin');
-admin.initializeApp();
 admin.initializeApp({
- credential: admin.credential.applicationDefault(),
- databaseURL: 'https://connexbot-sfb9-default-rtdb.firebaseio.com/', 
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'ws://connexbot-sfb9-default-rtdb.firebaseio.com/',
 });
- 
- 
+admin.initializeApp();
+const db = admin.firestore();
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
  
   function getBgHandler(agent) {
-    let bg = agent.parameters.name;
+    let bg = agent.parameters.name || agent.context.get('Starthealthcheck-followup').parameters.name;
+    db.collection('bg').add({ bg: bg });
     agent.add(`Your blood glucose level is ${bg}`);
   }
   
  function getSbpHandler(agent) {
-    let Sbp = agent.parameters.name;
+    let Sbp = agent.parameters.name || agent.context.get('Starthealthcheck-custom-followup').parameters.name; 
+   	db.collection('Sbp').add({ Sbp: Sbp });
     agent.add(`Your systolic blood pressure is ${Sbp}`);
   }
  
   
   function getDbpHandler(agent) {
-    let Dbp = agent.parameters.name;
+    let Dbp = agent.parameters.name || agent.context.get('Starthealthcheck-custom-custom-followup').parameters.name; 
+    db.collection('Dbp').add({ Dbp: Dbp });
     agent.add(`Your diastolic blood pressure is ${Dbp}`);
   }
   
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
-
   let intentMap = new Map();
   intentMap.set('bgGET', getBgHandler);
   intentMap.set('systolicGET', getSbpHandler);
   intentMap.set('diastolicGET', getDbpHandler);
-  intentMap.set('fallback', fallback);
   agent.handleRequest(intentMap);
 });
